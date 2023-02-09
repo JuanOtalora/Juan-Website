4be9d10731c4b17bc2f516042a40a5fb7042ab9b
JUAN <juanpocsc@gmail.com>
2023-02-08 22:42:33 -0500

    Updates

---
+++ precache-manifest.d5901a9dafba659fbd0ae0161a109e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd33e76a1dcde96516f1",
+    "url": "/Juan-Website/static/css/main.964c1f64.chunk.css"
+  },
+  {
+    "revision": "cd33e76a1dcde96516f1",
+    "url": "/Juan-Website/static/js/main.cd33e76a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Juan-Website/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/Juan-Website/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "37d5f4ba88955e43b462",
+    "url": "/Juan-Website/static/js/runtime~main.37d5f4ba.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Juan-Website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3e1c72753a2b14e303717d5cfc840ec4",
+    "url": "/Juan-Website/index.html"
+  }
+];
